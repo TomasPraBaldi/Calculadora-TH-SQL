5ef4a2ae147f9132a4e32267ceaeff9d9506ff33
TomasPraBaldi <117479258+TomasPraBaldi@users.noreply.github.com>
2023-09-21 19:17:46 -0300

    fff

--- backend calculadora .py
+++ backend calculadora .py
@@ -1,12 +1,25 @@
-valor1 = input("Digite o primeiro valor: ")
-operador = input("Digite um operador matemático: + , - , / , ou *: ")
-valor2 = input("Digite o segundo valor: ")
+valor1 = input("First value: ")
+if valor1.isdigit(): 
+    print()
+else:
+    print("This calculator can't use letters!")
+operador = input("Operator: + , - , / , or *: ")
+valor2 = input("Second value: ")
+if valor2.isdigit():
+    print()
+else:
+    print("This calculator can't use letters!")
 
 
-#NAO TA CERTO, NAO TA VALIDANDO O PRIMEIRO IF COMO TRUE...
-if valor1.isdigit() or valor2.isdigit():
-    print('digite um numero: ')
-elif operador == "+":
+#TENTATIVA 1 PRA DAR UMA MENSAGEM SE O USER DIGITAR STR
+#if valor1.isdigit() or valor2.isdigit():
+#    print('digite um numero: ')
+
+#TENTATIVA 2 PRA DAR UMA MENSAGEM SE O USER DIGITAR STR
+#if isinstance(True, (str)):
+#   print('digite um numero: ')
+
+if operador == "+":
     print(valor1+valor2)
 elif operador == "-":
     print(valor1-valor2)
@@ -15,13 +28,13 @@ elif operador == "/":
 elif operador == "*":
     print(valor1*valor2)
 else:
-    print("Digite algo valido.")
+    print("Digite um operador valido.")
 
 
 '''
 value = input("Enter Value: ")
 if value.isnumeric():
-  print("Input value is Integer!")
+    print("Input value is Integer!")
 else:
-  print("Input value is Not an Integer!")
+    print("Input value is Not an Integer!")
 '''
