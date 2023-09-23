557000f94d2d94c75e5166987f0f7799041128c3
tomasprabaldi <tom.ide.musica@gmail.com>
2023-09-23 12:52:47 -0300

    including def

--- calculator backEND.py
+++ calculator backEND.py
@@ -1,23 +1,29 @@
-valor1 = input("First value: ")
-if valor1.isdigit(): 
-    print()
-else:
-    print("This calculator can't use letters!")
-operador = input("Operator: + , - , / , or *: ")
+
+def val1():
+    global valor1
+    #valor1 = float(int(input("First value: ")))
+    valor1 = input("First value: ")
+    if valor1.isdigit(): 
+        print()
+    else:
+        print("This calculator can't use letters!")
+        val1()
+val1()
+
+def op():
+    global operador
+    operador = input("Operator: + , - , / , or *: ")
+    if operador !="+" and operador !="-" and operador !="*" and operador != "/":   
+        print("Type an valid operator!")
+        op()
+op()
+
 valor2 = input("Second value: ")
 if valor2.isdigit():
     print()
 else:
     print("This calculator can't use letters!")
 
-
-#TENTATIVA 1 PRA DAR UMA MENSAGEM SE O USER DIGITAR STR
-#if valor1.isdigit() or valor2.isdigit():
-#    print('digite um numero: ')
-
-#TENTATIVA 2 PRA DAR UMA MENSAGEM SE O USER DIGITAR STR
-#if isinstance(True, (str)):
-#   print('digite um numero: ')
 
 if operador == "+":
     print(valor1+valor2)
@@ -30,11 +36,3 @@ elif operador == "*":
 else:
     print("Digite um operador valido.")
 
-
-'''
-value = input("Enter Value: ")
-if value.isnumeric():
-    print("Input value is Integer!")
-else:
-    print("Input value is Not an Integer!")
-'''
